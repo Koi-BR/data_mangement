21c59a57b25f98843ab7da8a93a13c8302d2ab21
Koi-BR <62640505+Koi-BR@users.noreply.github.com>
2021-04-06 13:20:04 +0200

    Add files via upload

---
+++ POG.py
@@ -0,0 +1,11 @@
+def pythag(number1, number2):
+    hippo = (number1**2 + number2**2)**(1/2)
+    print("here is your big side" , hippo)
+
+number1 = int(input("Gibe one number please? "))
+number2 = int(input("Gibe another number please? "))
+
+pythag(number1, number2)
+
+#POOOOOOOOOOOOOOOOOOOOOOOOOG 
+
